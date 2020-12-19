f8ddac5bba1c123ba2c1f02dd5d4bb90281bf081
CLRminded <the_rocket_scientist@icloud.com>
2020-12-19 00:52:49 -0800

    playing around with vapor portfolio

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,14 +4,11 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req -> EventLoopFuture<View> in
-
-        struct WelcomeContext: Encodable {
-            var title:String
-            var name:String
-            var job:String
-        }
-
-        return req.view.render("index", WelcomeContext(title:"Vapor", name: "Chris", job:"Programmer"))
+        
+        return req.view.render("index", [
+                                "title":"Vapor",
+                                "name":"Chris",
+                                "job":"Programmer"])
     }
     
     app.get("hello") { req -> EventLoopFuture<View> in
